4fc8ddc839a44916209fa2ac3fdaa996c821b298
Teo1112 <t.puiu@yahoo.com>
2024-05-16 16:17:32 +0300

    al treilea exercitiu rezolvat

---
+++ Ex3/alTreileaExercitiu.js
@@ -0,0 +1,6 @@
+function inversareSir(sir){
+    return sir.reverse();
+}
+
+console.log(inversareSir([1, -5, 20, -34, 16, 29, 36, -4]))
+console.log(inversareSir([-100, -5, -2, 34, 50, 90, 95, 100]))
